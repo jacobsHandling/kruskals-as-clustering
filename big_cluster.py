d7efcdc1075a5000c51a3e62a12d65324cb5b7f7
Jacob Shandling <jacobshandling@icloud.com>
2021-05-06 14:55:13 -0700

    Make default ppath joining intelligent

--- big_cluster.py
+++ big_cluster.py
@@ -6,7 +6,7 @@ import time
 class BigCluster():
 
 
-    def __init__(self, filename: str = os.path.dirname(os.path.abspath(__file__)) + '/clustering_big.txt', min_spacing: int = 3):  # by default, looks for the input in file "clustering_big.txt" in the same directory as this script
+    def __init__(self, filename: str = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'clustering_big.txt'), min_spacing: int = 3):  # by default, looks for the input in file "clustering_big.txt" in the same directory as this script
 
         self.edge_costs = []
         self.edge_endpoints = {}  # { edge_cost: {(endpoint, endpoint)} } - still allows for (and there will be) duplicate edges with endpoints reversed - doesn't matter because the 2 endpoints will be unioned the first time their edge is considered, meaning the 2nd will be overlooked
